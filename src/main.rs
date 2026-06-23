7339e49ee883aeffae76b05530cfc89421fdcf12
xiao <xiao@qianyan.com>
2026-06-23 16:02:16 +0800

    fix: 移除 main.rs 中未使用的 setup_autostart/remove_autostart 死代码

--- src/main.rs
+++ src/main.rs
@@ -792,32 +792,3 @@ fn spawn_gui_null_handler(gui_rx: std::sync::mpsc::Receiver<GuiEvent>) {
     });
 }
 
-#[cfg(target_os = "windows")]
-pub fn setup_autostart() -> Result<(), Box<dyn std::error::Error>> {
-    let exe = std::env::current_exe()?;
-    let exe_path = exe.to_str().ok_or("Invalid path")?;
-    let _ = std::process::Command::new("reg")
-        .arg("add")
-        .arg("HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Run")
-        .arg("/v")
-        .arg("QianyanIME")
-        .arg("/t")
-        .arg("REG_SZ")
-        .arg("/d")
-        .arg(exe_path)
-        .arg("/f")
-        .status();
-    Ok(())
-}
-
-#[cfg(target_os = "windows")]
-pub fn remove_autostart() -> Result<(), Box<dyn std::error::Error>> {
-    let _ = std::process::Command::new("reg")
-        .arg("delete")
-        .arg("HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Run")
-        .arg("/v")
-        .arg("QianyanIME")
-        .arg("/f")
-        .status();
-    Ok(())
-}
